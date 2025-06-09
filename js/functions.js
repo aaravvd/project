dab61153f8e1aaff6bade82117444d0813b1d46b
aaravvd <140924079+aaravvd@users.noreply.github.com>
2025-06-08 23:36:31 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -92,27 +92,37 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+const offsetMilliseconds = (2 * 365 * 24 * 60 * 60 + 22 * 3600 + 30 * 60) * 1000; // Approx. 2 years + 22h30m
+const startDate = new Date(Date.now() - offsetMilliseconds); // Adjusted start date
+
+function timeElapse(date) {
+	var current = new Date();
+	var seconds = Math.floor((current - Date.parse(date)) / 1000);
+
 	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
+	seconds %= (3600 * 24);
+
 	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
+	if (hours < 10) hours = "0" + hours;
+	seconds %= 3600;
+
 	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
-	$("#elapseClock").html(result);
+	if (minutes < 10) minutes = "0" + minutes;
+	seconds %= 60;
+
+	if (seconds < 10) seconds = "0" + seconds;
+
+	var result = 
+		"<span class=\"digit\">" + days + "</span> days " +
+		"<span class=\"digit\">" + hours + "</span> hours " +
+		"<span class=\"digit\">" + minutes + "</span> minutes " +
+		"<span class=\"digit\">" + seconds + "</span> seconds";
+
+	document.getElementById("elapseClock").innerHTML = result;
 }
+
+// Update every second
+setInterval(() => timeElapse(startDate), 1000);
 
 function showMessages() {
 	adjustWordsPosition();
